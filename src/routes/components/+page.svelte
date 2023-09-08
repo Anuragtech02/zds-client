bb7881a890ba3bdcfd8494760da7f30c39c31565
Anurag Pal <apal895@gmail.com>
2023-09-09 01:30:47 +0530

    added header

--- src/routes/components/+page.svelte
+++ src/routes/components/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import Header from '$lib/components/Header/Header.svelte';
 	import NavLink from '$lib/components/Header/NavLink.svelte';
 	import { Button, Input, Logo, MetaverseButton } from '$lib/components/index';
 </script>
 
 <section class="max-width-container [&>div]:my-4">
+	<Header />
 	<div>
 		<Button>Click me</Button>
 	</div>

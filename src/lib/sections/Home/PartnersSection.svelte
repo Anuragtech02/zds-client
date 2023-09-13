7cbd02259a931da00aa5e209c63b6cfce5eccf75
Anurag Pal <apal895@gmail.com>
2023-09-14 03:54:54 +0530

    sligtly for ourpartners section

--- src/lib/sections/Home/PartnersSection.svelte
+++ src/lib/sections/Home/PartnersSection.svelte
@@ -1,7 +1,23 @@
 <script>
+	import OutlinedText from '$lib/components/OutlinedText.svelte';
 	import SectionLayout from '$lib/layout/SectionLayout.svelte';
 </script>
 
 <div>
-	<SectionLayout>Partners Section</SectionLayout>
+	<SectionLayout>
+		<h3 class="uppercase">
+			Partners
+			<OutlinedText text="we&apos;ve worked with" />
+		</h3>
+		<div class="w-full rounded-xl border-2 border-borders-500 p-10 mt-8">
+			<div class="flex flex-wrap justify-betwen items-center gap-8">
+				<div class="w-[300px] h-[150px] bg-white/100" />
+				<div class="w-[300px] h-[150px] bg-white/100" />
+				<div class="w-[300px] h-[150px] bg-white/100" />
+				<div class="w-[300px] h-[150px] bg-white/100" />
+				<div class="w-[300px] h-[150px] bg-white/100" />
+				<div class="w-[300px] h-[150px] bg-white/100" />
+			</div>
+		</div>
+	</SectionLayout>
 </div>

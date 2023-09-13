867ab893ad7834156dff0ba23b9c77a48b2a2484
Anurag Pal <apal895@gmail.com>
2023-09-14 03:31:54 +0530

    hero bsaic structure done

--- src/lib/sections/Home/Hero.svelte
+++ src/lib/sections/Home/Hero.svelte
@@ -1,5 +1,25 @@
 <script>
+	import Button from '$lib/components/Button.svelte';
+	import OutlinedText from '$lib/components/OutlinedText.svelte';
 	import SectionLayout from '$lib/layout/SectionLayout.svelte';
 </script>
 
-<SectionLayout className="h-screen">This is Hero</SectionLayout>
+<SectionLayout type="wrapper">
+	<div class="max-width-container">
+		<div class="flex flex-col justify-between items-start h-[60vh]">
+			<div class="flex justify-between items-end gap-10 w-full">
+				<div>
+					<h1 class="text-left uppercase">
+						<span>Engage.</span> <br />
+						<OutlinedText text="Entertain." className="mt-4" /> <br />
+						<OutlinedText text="Evolve." className="mt-4" /> <br />
+					</h1>
+				</div>
+				<p class="max-w-[400px] px-4 text-left mt-8 border-l-2">
+					Translating Ideas & Imaginations Into Creative Audio-Visual Communication Experiences.
+				</p>
+			</div>
+			<Button>Our Work</Button>
+		</div>
+	</div>
+</SectionLayout>

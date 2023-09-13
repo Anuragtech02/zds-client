974578d8b4504da22ff73489b8f0f3ad459acb1d
Rishi <grishi634@gmail.com>
2023-09-14 00:06:11 +0530

    second section completed

---
+++ src/lib/components/HomeSecondSection.svelte
@@ -0,0 +1,65 @@
+<div class="second-section-home flex flex-col justify-around min-h-screen p-16">
+	<div class="mb-auto sm:mb-0 self-start">
+		<p class="text-left md:text-[1.5rem] lg:text-[2rem]">We are Hybrid of</p>
+		<h2 class="text-left">Creative Agency &</h2>
+		<span class="gradient-line" />
+	</div>
+	<div class="mb-auto sm:mb-0 self-end flex flex-col z-[3]">
+		<h2 class="text-right sm:text-left">Content</h2>
+		<h2 class="text-right sm:text-left">Production Studio</h2>
+		<span class="gradient-line self-end" />
+	</div>
+	<a href="/" class="show-reel-btn z-[3]"> Show Reel </a>
+	<div class="background-image-vr" />
+</div>
+
+<style>
+	.second-section-home {
+		position: relative;
+		overflow: hidden;
+	}
+	.background-image-vr {
+		z-index: 1;
+		background: url(/images/personVR.png) no-repeat bottom;
+		background-size: contain;
+		height: 75%;
+		width: 100%;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+	}
+	.show-reel-btn {
+		align-self: center;
+		padding: 1rem 3rem;
+		border-radius: 50px;
+		border: 1px solid #fff;
+		backdrop-filter: blur(10px);
+	}
+	.gradient-line {
+		display: inline-block;
+		width: 20%;
+		height: 5px;
+		background: linear-gradient(to right, #ff439d, #2518d1);
+	}
+	@media screen and (max-width: 1100px) {
+		.background-image-vr {
+			background-size: cover;
+		}
+	}
+	@media screen and (max-width: 500px) {
+		.second-section-home div h2 {
+			font-size: 1.5rem;
+		}
+		.second-section-home {
+			padding: 2rem;
+		}
+	}
+	@media screen and (max-width: 330px) {
+		.second-section-home div h2 {
+			font-size: 1.5rem;
+		}
+		.second-section-home {
+			padding: 2rem;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
+	import HomeSecondSection from '$lib/components/HomeSecondSection.svelte';
 	import Slider from '$lib/components/Slider.svelte';
 </script>
 
-<div style="height: 50vh;">
-	<Slider />
-</div>
+<HomeSecondSection />

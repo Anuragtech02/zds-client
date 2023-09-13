72477d5791efe45b18f4081e3f8f4830dbc99063
Anurag Pal <apal895@gmail.com>
2023-09-14 01:52:10 +0530

    BannerCTA complete

--- src/lib/sections/Home/BannerCTA.svelte
+++ src/lib/sections/Home/BannerCTA.svelte
@@ -1,5 +1,11 @@
 <script>
+	import Button from '$lib/components/Button.svelte';
 	import SectionLayout from '$lib/layout/SectionLayout.svelte';
 </script>
 
-<SectionLayout>Banner CTA</SectionLayout>
+<SectionLayout type="wrapper" className="gradient-1">
+	<div class="max-width-container flex justify-between items-center flex-col sm:flex-row">
+		<h3 class="uppercase sm:text-left">Liked our work? let&apos;s work together!</h3>
+		<Button className="whitespace-nowrap mt-4 sm:mt-0">Contact Us</Button>
+	</div>
+</SectionLayout>

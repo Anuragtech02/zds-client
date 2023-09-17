c7b4c9a1dfd0d6b849bd1c890f4bc18480ac13f4
Rishi <grishi634@gmail.com>
2023-09-17 19:32:04 +0530

    work page done

---
+++ src/lib/sections/Work/WorkVideo.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let heading = 'Lorem Ipsum Dolor';
+	let category = 'Animation & Visual Packaging';
+	let src = '/sample_videos/sample_video.mp4';
+</script>
+
+<div class="h-full lg:w-[47%] relative">
+	<p class="absolute top-0 left-0 w-full p-2 px-4 text-left backdrop-blur rounded-t-xl">
+		{heading}
+	</p>
+	<video {src} loop autoplay muted class="rounded-xl object-cover" />
+	<p class="absolute bottom-2 left-2 rounded-2xl px-4 py-1 backdrop-blur">
+		{category}
+	</p>
+</div>
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,2 +1,62 @@
 <script lang="ts">
+	import WorkVideo from '$lib/sections/Work/WorkVideo.svelte';
+
+	let categories = [
+		'All Categories',
+		'web',
+		'design',
+		'illustration',
+		'branding',
+		'photography',
+		'app'
+	];
+	let selectedCategory = categories[0];
+	let categoryChangeHandler = (category: string) => {
+		selectedCategory = category;
+	};
 </script>
+
+<div class="min-h-screen flex flex-col justify-start items-start gap-12">
+	<div
+		class="w-full flex justify-start md:justify-around gap-4 items-center border-b-2 pb-4 overflow-x-scroll lg:overflow-hidden"
+	>
+		{#each categories as category}
+			<button
+				class={`${
+					category === selectedCategory ? 'active-category' : ''
+				} text-lg w-fit cursor-pointer relative whitespace-nowrap`}
+				on:click={categoryChangeHandler.bind(null, category)}
+			>
+				{category}
+			</button>
+		{/each}
+	</div>
+	<div class="lg:w-[60%] text-lg">
+		Welcome to the heart of our creative journey- a showcase of our finest work across diverse
+		services. Explore our portfolio, where each project represents, not just our expertise, but our
+		unwavering dedication to turning ideas into captivating realities.
+	</div>
+	<div class="lg:w-[60%] text-lg">
+		Our portfolio is a testament to our versatility, innovation, and dedication. Each project is a
+		unique story of a collaborative effort, creativity, and unrestrained enthusiasm. Join us as we
+		take you on a visual journey through our diverse range of projects.
+	</div>
+	<div class="flex flex-wrap justify-center items-center lg:justify-start gap-8">
+		{#each [1, 2, 3, 4, 5] as video}
+			<WorkVideo />
+		{/each}
+	</div>
+</div>
+
+<style>
+	.active-category::after {
+		content: '';
+		display: block;
+		width: 7px;
+		height: 7px;
+		position: absolute;
+		left: 45%;
+		border-radius: 100%;
+		background-color: #ff00ce;
+	}
+</style>

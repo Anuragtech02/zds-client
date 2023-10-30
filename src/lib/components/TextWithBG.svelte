e0c5d9792570dd184e064d6c065d472e0b53dd41
Anurag Pal <apal895@gmail.com>
2023-10-30 20:38:56 +0530

    custom band animation complete

--- src/lib/components/TextWithBG.svelte
+++ src/lib/components/TextWithBG.svelte
@@ -1,8 +1,44 @@
 <script lang="ts">
+	import { gsap } from 'gsap';
+	import { ScrollTrigger } from 'gsap/ScrollTrigger';
+	import { onMount } from 'svelte';
+
 	export let className: string = '';
+
+	gsap.registerPlugin(ScrollTrigger);
+	let customBand: any = null;
+	let bandContainer: any = null;
+
+	onMount(() => {
+		if (customBand) {
+			setTimeout(() => {
+				gsap.fromTo(
+					customBand,
+					{
+						width: '5%'
+					},
+					{
+						width: '105%',
+						duration: 1,
+						ease: 'power2.out',
+						scrollTrigger: {
+							trigger: bandContainer,
+							start: 'top bottom',
+							end: 'bottom top',
+							toggleActions: 'play reverse play reverse'
+						}
+					}
+				);
+			}, 300);
+		}
+	});
 </script>
 
-<span class={`font-dela-gothic inline-block bg-gradient-band z-[1] relative ${className}`}>
+<span
+	bind:this={bandContainer}
+	class={`font-dela-gothic inline-block bg-gradient-band z-[1] relative ${className}`}
+>
+	<span bind:this={customBand} class="custom-bg-band" />
 	<slot />
 </span>
 
@@ -11,14 +47,13 @@
 		position: relative;
 		z-index: 1;
 	}
-	.bg-gradient-band::after {
-		content: '';
+	.custom-bg-band {
 		position: absolute;
-		bottom: 15%;
-		left: 0;
-		width: 100%;
+		width: 5%;
 		height: 40%;
 		z-index: -1;
+		bottom: 10%;
+		overflow: hidden;
 		@apply gradient-1;
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
-<script>
+<script lang="ts">
 	import BaseLayer from '$lib/layout/BaseLayer.svelte';
+	import Lenis from '@studio-freight/lenis';
 	import '../app.css';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const lenis = new Lenis();
+
+		// lenis.on('scroll', (e: any) => {
+		// 	console.log(e);
+		// });
+
+		function raf(time: number) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+	});
 </script>
 
 <BaseLayer>

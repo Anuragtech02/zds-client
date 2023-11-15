8775400275d5af99c8437e54dcad67767c2b6c16
Anurag Pal <apal895@gmail.com>
2023-11-15 12:12:14 +0530

    description divided

--- src/lib/sections/Home/Hero.svelte
+++ src/lib/sections/Home/Hero.svelte
@@ -9,26 +9,21 @@
 
 	export let data: any;
 
-	let descriptionEl: HTMLDivElement;
+	let container: HTMLDivElement;
 	let activeWordIdx = 0;
-	let words: string[] = [];
+	let heading: string[] = [];
+	let description: string[] = [];
 	let Title: string, Description: string, CTAText: string, CTALink: string, Background_Video;
 	// const { Title, Description, CTAText, CTALink, Background_Video } = data;
 	// console.log(data);
-	if (data) {
-		Title = data.Title;
-		Description = data.Description;
-		CTAText = data.CTAText;
-		CTALink = data.CTALink;
-		Background_Video = data.Background_Video;
-	} else {
-		Title = '';
-		Description = '';
-		CTAText = '';
-		CTALink = '';
-		Background_Video = '';
-	}
-	words = Title.split(',');
+	Title = data?.Title || '';
+	Description = data?.Description || '';
+	CTAText = data?.CTAText || '';
+	CTALink = data?.CTALink || '';
+	Background_Video = data?.Background_Video || '';
+
+	heading = Title.split(',');
+	description = Description.split(',');
 	onMount(() => {
 		gsap.from('.text-reveal', 1.8, {
 			y: 100,
@@ -58,17 +53,14 @@
 
 		// timer to update active word
 		setInterval(() => {
-			activeWordIdx = activeWordIdx === words.length - 1 ? 0 : activeWordIdx + 1;
+			activeWordIdx = activeWordIdx === heading.length - 1 ? 0 : activeWordIdx + 1;
 			// change the margin to pand bottom of .left-reveal using gsap
-			if (activeWordIdx === 0) {
-				descriptionEl.style.marginTop === '0';
-				descriptionEl.style.marginBottom === 'auto';
-			} else if (activeWordIdx === 1) {
-				descriptionEl.style.marginTop === 'auto';
-				descriptionEl.style.marginBottom === 'auto';
-			} else if (activeWordIdx === 2) {
-				descriptionEl.style.marginTop === 'auto';
-				descriptionEl.style.marginBottom === '0';
+			if (activeWordIdx === 0 && container?.style) {
+				// container.style.alignItems = 'flex-start';
+			} else if (activeWordIdx === 1 && container?.style) {
+				// container.style.alignItems = 'center';
+			} else if (activeWordIdx === 2 && container?.style) {
+				// container.style.alignItems = 'flex-end';
 			}
 		}, 3000);
 	});
@@ -82,21 +74,21 @@
 	/>
 	<div class="max-width-container relative z-[1] flex justify-between items-center h-screen">
 		<div class="flex flex-col justify-between items-start w-full">
-			<div class="flex justify-start items-start sm:items-end flex-col sm:flex-row w-full">
+			<div class="flex justify-start items-stretch flex-col sm:flex-row w-full">
 				<div class="w-full uppercase flex flex-col justify-center items-start">
 					<div class="relative h-[70px] md:h-[80px] lg:h-[100px] w-full overflow-hidden">
 						<h1 class="text-reveal text-left absolute">
 							<OutlinedText
-								text={words[0]}
+								text={heading[0]}
 								className="text-reveal"
 								isActive={activeWordIdx === 0}
 							/> <br />
 						</h1>
 					</div>
-					<div class="relative md:mt-12 h-[80px] md:h-[100px] xl:h-[140px] w-full overflow-hidden">
+					<div class="relative md:mt-8 h-[80px] md:h-[100px] xl:h-[140px] w-full overflow-hidden">
 						<h1 class="text-reveal text-left absolute">
 							<OutlinedText
-								text={words[1]}
+								text={heading[1]}
 								className="text-reveal"
 								isActive={activeWordIdx === 1}
 							/> <br />
@@ -105,17 +97,47 @@
 					<div class="relative md:mt-4 h-[80px] md:h-[100px] xl:h-[130px] w-full overflow-hidden">
 						<h1 class="text-reveal text-left absolute uppercase">
 							<OutlinedText
-								text={words[2]}
+								text={heading[2]}
 								className="text-reveal"
 								isActive={activeWordIdx === 2}
 							/> <br />
 						</h1>
 					</div>
 				</div>
-				<div class="overflow-hidden border-l-2 max-w-[400px] mt-10 sm:mt-0 md:ml-20">
-					<p class="left-reveal px-4 text-left w-full" bind:this={descriptionEl}>
-						{Description}
-					</p>
+				<div class="flex flex-col justify-between w-full">
+					<div class="flex justify-start items-center h-full w-full">
+						<div
+							class="overflow-hidden border-l-2 max-w-[400px] w-full mt-10 sm:mt-0 md:ml-20 transition-all duration-300 ease-out"
+							class:opacity-0={activeWordIdx !== 0}
+							class:opacity-100={activeWordIdx === 0}
+						>
+							<p class="left-reveal px-4 text-left w-full min-w-[100%]">
+								{description[activeWordIdx]}
+							</p>
+						</div>
+					</div>
+					<div class="flex justify-start items-center h-full w-full">
+						<div
+							class="overflow-hidden border-l-2 max-w-[400px] w-full mt-10 sm:mt-0 md:ml-20 transition-all duration-300 ease-out"
+							class:opacity-0={activeWordIdx !== 1}
+							class:opacity-100={activeWordIdx === 1}
+						>
+							<p class="left-reveal px-4 text-left w-full min-w-[100%]">
+								{description[activeWordIdx]}
+							</p>
+						</div>
+					</div>
+					<div class="flex justify-start items-center h-full w-full">
+						<div
+							class="overflow-hidden border-l-2 max-w-[400px] w-full mt-10 sm:mt-0 md:ml-20 transition-all duration-300 ease-out"
+							class:opacity-0={activeWordIdx !== 2}
+							class:opacity-100={activeWordIdx === 2}
+						>
+							<p class="left-reveal px-4 text-left w-full min-w-[100%]">
+								{description[activeWordIdx]}
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 			<Button

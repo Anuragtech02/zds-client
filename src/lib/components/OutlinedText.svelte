2cb7741c07564f27ed203eb4da3d835c74032598
Anurag Pal <apal895@gmail.com>
2023-11-15 17:19:09 +0530

    new animation hero text

--- src/lib/components/OutlinedText.svelte
+++ src/lib/components/OutlinedText.svelte
@@ -7,9 +7,13 @@
 
 <!-- <span class=" relative"> -->
 <span
-	class={`inline-block font-dela-gothic outlined-text ${className} ${isActive ? 'active' : ''}`}
+	class={`relative inline-block font-dela-gothic outlined-text ${className} ${
+		isActive ? 'active-text' : ''
+	}`}
 	class:w-stroke-1={strokeWidth === 'w-stroke-1'}
 	class:w-stroke-2={strokeWidth === 'w-stroke-2'}
+	data-fill-text={text}
+	class:active-text={isActive}
 >
 	{text}
 </span>
@@ -24,11 +28,27 @@
 		-webkit-text-stroke: 2px white;
 	}
 	.outlined-text {
-		-webkit-text-fill-color: transparent;
+		/* -webkit-text-fill-color: transparent; */
+		color: transparent;
 		stroke-linejoin: round;
 		/* transition: all 0.3s ease-in-out; */
 	}
-	.active {
+	/* .active {
 		-webkit-text-fill-color: white;
+	} */
+	/* .outlined-text {
+	} */
+	.outlined-text::before {
+		content: attr(data-fill-text);
+		position: absolute;
+		width: 0;
+		height: 100%;
+		color: #ffffff;
+		-webkit-text-stroke: 0px transparent;
+		overflow: hidden;
+		transition: width 0.8s cubic-bezier(0.165, 0.84, 0.44, 1);
+	}
+	.active-text::before {
+		width: 100%;
 	}
 </style>
